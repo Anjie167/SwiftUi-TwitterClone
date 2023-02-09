1c604c206da3a19c232602a04a0839efa5f9277d
Anjola favour ayomikun <anjie167@gmail.com>
2023-02-09 17:47:06 +0100

    Initial Commit

---
+++ SwiftUi-TwitterClone/SwiftUi_TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_TwitterCloneApp.swift
+//  SwiftUi-TwitterClone
+//
+//  Created by FAVOUR ANJOLA on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
